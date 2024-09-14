083933261edeeced815862da0f476b9896e5f9df
Ubuntu <ubuntu@ip-172-31-79-231.ec2.internal>
2024-09-14 16:41:57 +0000

    final update

--- docker_file.sh
+++ docker_file.sh
@@ -4,7 +4,7 @@
 #This Script is using by Docker installation and container run 
 ##############################################################
 #update the packages list 
-echo "update the packages
+echo "update the packages"
 sudo apt update
 
 #install the docker 
@@ -13,20 +13,23 @@ sudo apt install docker.io -y
 
 #check the docker version 
 echo"list the docker version"
-docker --version
+sudo docker --version
 
 #pull the docker images
 echo"pulling httpd images"
-docker pull httpd 
+sudo docker pull httpd 
 
 #show the docker images
 echo"listing the docker images"
-docker images
+sudo docker images
 
 #run the container 
 echo"Running the httpd container"
-docker run -d --name maikel -p "8080:8080" httpd
+sudo docker run -d --name maikeraj -p "8090:80" httpd
 
 #wait for 2 minutes
 echo "Container is running. Waiting for 2 minutes..."
-sleep 2
+sleep 120
+#stop the conatiner 
+echo "Stopping the container..."
+sudo docker stop maikel
